a7b98b15b89d15faec04f132c876764370fe72fa
xtam-sys <bramyuniarso19@gmail.com>
2026-01-21 00:14:06 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cryptvault-v2';
+const CACHE = 'cv3';
 const FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
